4b49a5c1f3e5a3f262e2fcb828c6774c20312423
Mateus Cerqueira <mateusjesus2309@gmail.com>
2025-10-20 12:48:43 +0100

    Main changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:projectId" element={<ProjectPage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Contact from "./pages/Contact.jsx";
-import ProjectPage from "./pages/ProjectPage.jsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App"; // ← Agora importa o App corrigido
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects/:projectId" element={<ProjectPage />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
